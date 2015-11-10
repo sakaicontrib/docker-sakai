8e2dbfab67f80ce3566f23585134c7cffb85c426
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2015-11-10 10:56:58 +0000

    Update to docker-machine for port forwarding.

--- forward-ports.sh
+++ forward-ports.sh
@@ -2,10 +2,22 @@
 # This forwards ports used in development so you can just work as it everything is
 # running locally
 
+if [ "$1" != "" ] ; then
+  machine="$1"
+else
+  machine=$(docker-machine active 2>/dev/null)
+  # If there's no machine active exit.
+  if [ $? -ne 0 ] ; then
+    echo "You need to supply a docker-machine host to use or set the active host"
+    exit 1;
+  fi
+fi
+
+
 ports="8000 3306 5400 8983 10001"
 public_ports="8080"
 
-echo "Forwaring ports (private: $ports, public: $public_ports), ^C to stop"
+echo "Forwaring ports (private: $ports, public: $public_ports) on $machine, ^C to stop"
 opts=""
 for port in $ports
 do
@@ -15,5 +27,5 @@ for port in $public_ports
 do
   opts="${opts} -L*:${port}:localhost:${port}"
 done
-boot2docker ssh $opts -N
+docker-machine ssh "$machine" $opts -N
 
